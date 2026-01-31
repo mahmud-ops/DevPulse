ba40858c63882f4573799e7b990838f307920b93
Abdullah Al Mahmud <abdullahmahmud01798@gmail.com>
2026-01-31 12:34:55 +0600

    Setup : Chakra ui + Files

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from '@chakra-ui/react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Button>Click me</Button>
+    </div>
   )
 }
 
 export default App
+
+/*src/
+ ├─ app/              // app setup (providers, layouts)
+ ├─ components/       // reusable UI components
+ ├─ pages/            // route-level pages
+ ├─ features/         // domain-specific logic (projects, tasks, etc.)
+ ├─ hooks/            // custom hooks
+ ├─ services/         // API logic
+ ├─ store/            // global state
+ ├─ types/            // shared TS types
+ ├─ utils/            // helper functions
+ └─ routes/           // route definitions
+*/
